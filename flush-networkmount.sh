09735b240e8c31e10b42fea309507dd6d1d71ab9
Glenn Wright <gwright@kx.com>
2018-04-17 09:48:08 +0100

    First github submission

---
+++ flush-networkmount.sh
@@ -0,0 +1,14 @@
+# an example with a network file system which hold its own cache.
+# You will need to change this script for your networked file system under test
+export WORKDIR=/mnt/kdb
+export FS=nfs:/targetdir
+MOUNT=mount.PLACEYOURNETWORKEDFS-HERE
+UMOUNT=mount.PLACEYOURNETWORKEDFS-HERE
+if [ `uname -s` = "Darwin" ]
+then
+        purge
+else
+	$UMOUNT $WORKDIR
+	sync; echo 3 > /proc/sys/vm/drop_caches
+	$MOUNT $FS $WORKDIR
+fi
