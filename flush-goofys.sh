0bde6f50bfcdb862852540d923145defa63cef7d
Glenn Wright <gwright@kx.com>
2018-04-17 09:48:07 +0100

    First github submission

---
+++ flush-goofys.sh
@@ -0,0 +1,15 @@
+# an example with a network file system which hold its own cache.
+# goofys is just an example here. You will need to change this script for yours
+
+# Goofy rules ok
+
+WORKDIR=/mnt
+if [ `uname -s` = "Darwin" ]
+then
+        purge
+else
+	sync; echo 3 > /proc/sys/vm/drop_caches
+	umount $WORKDIR
+	sudo -u ec2-user /home/ec2-user/goofys/goofys -o nonempty,allow_other kxs3fs-test $WORKDIR
+
+fi
