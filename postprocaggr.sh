ffbcc280c021a82943cbd37ead59089f577c2b62
FerencBodon-Kx <fbodon@kx.com>
2022-01-13 12:19:32 +0000

    displaying Write Rate in final CSV

--- postprocaggr.sh
+++ postprocaggr.sh
@@ -10,12 +10,13 @@ if [ ! -f $AGGRFILE ]; then
 fi
 
 
-echo 'numproc,Streaming Read,random64k,random64ku,random1m,random1mu'
+echo 'numproc,Write Rate,Streaming Read,random64k,random64ku,random1m,random1mu'
 NUNMPROC=$(grep numprocs ${AGGRFILE} | cut -d " " -f 2)
+WRITERATE=$(grep 'Total Write Rate(sync)' ${AGGRFILE} | cut -d " " -f 5)
 STREAMINGRATE=$(grep 'Streaming Read(mapped) Rate' ${AGGRFILE} | cut -d " " -f 5)
 RAND64K=$(grep "random64k " ${AGGRFILE} | cut -d " " -f 5)
 RAND64KU=$(grep "random64ku " ${AGGRFILE} | cut -d " " -f 5)
 RAND1M=$(grep "random1m " ${AGGRFILE} | cut -d " " -f 5)
 RAND1MU=$(grep "random1mu " ${AGGRFILE} | cut -d " " -f 5)
 
-echo ${NUNMPROC},${STREAMINGRATE%.*},${RAND64K%.*},${RAND64KU%.*},${RAND1M%.*},${RAND1MU%.*}
+echo ${NUNMPROC},${WRITERATE},${STREAMINGRATE%.*},${RAND64K%.*},${RAND64KU%.*},${RAND1M%.*},${RAND1MU%.*}
