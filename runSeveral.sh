df6fb7acaabe2820fccb1a98e445bf0aae60f38c
FerencBodon-Kx <fbodon@kx.com>
2022-01-13 11:46:25 +0000

    adding a post-processing script and a runner that tests multiple setups

---
+++ runSeveral.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DIRPREFIX=$(date +%m%d:)
+echo "cleaning ${DIRPREFIX}*"
+rm -rf ${DIRPREFIX}*
+
+for i in {1,2,4,8,16,32,64,96}; do
+   ./mthread.sh $i delete
+done
+
+for i in ${DIRPREFIX}*; do
+	./postprocaggr.sh $i/aggregates-$(hostname) > $i/aggr.csv;
+done
+
+cat ${DIRPREFIX}*/aggr.csv | sort -n| uniq > aggr_total.csv
