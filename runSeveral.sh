e5efd24da40c7d9355e1314b444aee505e67ec79
Ferenc Bodon <fbodon@kx.com>
2024-10-04 13:57:17 +0200

    KXI-54226 naming fixes

--- runSeveral.sh
+++ runSeveral.sh
@@ -12,15 +12,15 @@ HOST=$(uname -n)
 DATES=()
 
 for i in {1,2,4,8,16,32,64,96}; do
-   DATE=$(date +%m%dD%H%M)
+   DATE=$(date +%m%d_%H%M)
    DATES+=($DATE)
    ./mthread.sh $i full delete ${DATE}
 done
 
-head -n 1 results/${DATES[1]}-${DATES[1]}/aggregate-$HOST.psv > ${OUTPUT}
+head -n 1 results/${DATES[1]}-${DATES[1]}/throughput-$HOST.psv > ${OUTPUT}
 TMP="$(mktemp)"
 for DATE in ${DATES[@]}; do
-   tail -n +2 results/${DATE}-${DATE}/aggregate-$HOST.psv >> ${TMP}
+   tail -n +2 results/${DATE}-${DATE}/throughput-$HOST.psv >> ${TMP}
 done
 
 sort ${TMP} -t '|' -k 3,3 >> ${OUTPUT}
